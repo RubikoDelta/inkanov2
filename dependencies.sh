450fb1d1b76fa974a5645475cd80c83f8184d24e
diego21i <diego.ag97@gmail.com>
2021-10-28 17:33:55 -0600

    Deoendencies
    se agrego dicho archivo

---
+++ dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+env/bin/pip install Flask
+env/bin/pip install Flask-Script
+env/bin/pip install Flask-Bootstrap4
+env/bin/pip install WTForms
+env/bin/pip install Flask-WTF
+env/bin/pip install Flask-SQLAlchemy
+env/bin/pip install Werkzeug
+env/bin/pip install flask-login
+env/bin/pip install WTForms-SQLAlchemy
+env/bin/pip install email_validator
+env/bin/pip install pytest-shutil
+env/bin/pip install pynput
+
+apt-get install python3-dev default-libmysqlclient-dev build-essential -y
+
+env/bin/pip install mysqlclient
